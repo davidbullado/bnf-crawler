0afba6de63e9b8c5f61251f43c3cc07a6e736a17
David Bullado <david.bullado@goldenbees.fr>
2023-01-05 18:49:13 +0100

    headless

--- main.py
+++ main.py
@@ -189,13 +189,14 @@ def europresse_find_title(driver, title):
 
 
 options = Options()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("start-maximized")
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
